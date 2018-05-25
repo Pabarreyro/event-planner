59dd15d5835bd60efbba347d8b9d1eb1fd149fb1
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 14:59:19 -0700

    Add test for setting name; refactor all tests to remove name from constructor

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -6,28 +6,35 @@ import static org.junit.Assert.*;
 
 public class EventTest {
     @Test
-    public void newEvent_instantiatesCorrectly_test() {
-        Event testEvent = new Event("test");
-        assertEquals("test", testEvent.getName());
+    public void newEvent_instantiatesCorrectly_true() {
+        Event testEvent = new Event();
+        assertEquals(true, testEvent instanceof Event);
+    }
+
+    @Test
+    public void setName_setsNameCorrectly_pablosBday() {
+        Event testEvent = new Event();
+        testEvent.setName("Pablo's Bday");
+        assertEquals("Pablo's Bday", testEvent.getName());
     }
 
     @Test
     public void setGuests_setsGuestsCorrectly_0to50() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setGuests("0-50");
         assertEquals("0-50", testEvent.getGuests());
     }
 
     @Test
     public void setFood_setsSingleFoodRequestCorrectly_horsDoeuvres() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setFood("hors d'oeuvres");
         assertEquals("hors d'oeuvres", testEvent.getFood().get(0));
     }
 
     @Test
     public void setFood_setsMultipleFoodRequestsCorrectly_2() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setFood("hors d'oeuvres");
         testEvent.setFood("buffet");
         assertEquals(2, testEvent.getFood().size());
@@ -35,14 +42,14 @@ public class EventTest {
 
     @Test
     public void setDrink_setsSingleDrinkRequestCorrectly_beerAndWine() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setDrink("beer & wine");
         assertEquals("beer & wine", testEvent.getDrink().get(0));
     }
 
     @Test
     public void setDrink_setsMultipleDrinkRequestsCorrectly_2() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setDrink("beer & wine");
         testEvent.setDrink("tea & coffee");
         assertEquals(2, testEvent.getDrink().size());
@@ -50,14 +57,14 @@ public class EventTest {
 
     @Test
     public void setEntertainment_setsSingleEntertainmentRequestCorrectly_liveBand() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setEntertainment("live band");
         assertEquals("live band", testEvent.getEntertainment().get(0));
     }
 
     @Test
     public void setEntertainment_setsMultipleEntertainmentRequestsCorrectly_2() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setEntertainment("live band");
         testEvent.setEntertainment("dj");
         assertEquals(2, testEvent.getEntertainment().size());
@@ -65,7 +72,7 @@ public class EventTest {
 
     @Test
     public void setPrice_setsPriceBasedOnGuests_400() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 400;
         testEvent.setGuests("50-100");
         testEvent.setPrice();
@@ -74,7 +81,7 @@ public class EventTest {
 
     @Test
     public void setPrice_setsPriceBasedOnFood_3400() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 3400;
         testEvent.setGuests("50-100");
         testEvent.setFood("hors d'oeuvres");
@@ -85,7 +92,7 @@ public class EventTest {
 
     @Test
     public void setPrice_setsPriceBasedOnDrink_1900() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 1900;
         testEvent.setGuests("50-100");
         testEvent.setDrink("beer & wine");
@@ -96,7 +103,7 @@ public class EventTest {
 
     @Test
     public void setPrice_setsPriceBasedOnEntertainment_2200() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 2200;
         testEvent.setGuests("50-100");
         testEvent.setEntertainment("live band");
@@ -107,21 +114,21 @@ public class EventTest {
 
     @Test
     public void setCoupon_setsValidDiscountCoupon_partyOn() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setCoupon("coming out");
         assertEquals("coming out", testEvent.getCoupon());
     }
 
     @Test
     public void setCoupon_doesNotSetInvalidDiscountCoupon_none() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         testEvent.setCoupon("I'm a fraud");
         assertEquals("none", testEvent.getCoupon());
     }
 
     @Test
     public void setPrice_doesNotApplyDiscountWhenCouponAbsent_400() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 400;
         testEvent.setGuests("50-100");
         testEvent.setPrice();
@@ -130,7 +137,7 @@ public class EventTest {
 
     @Test
     public void setPrice_appliesValidFlatDiscountToPrice_750() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 750;
         testEvent.setGuests("150-200");
         testEvent.setCoupon("party on");
@@ -140,7 +147,7 @@ public class EventTest {
 
     @Test
     public void setPrice_appliesValidProportionalDiscountToPrice_320() {
-        Event testEvent = new Event("test");
+        Event testEvent = new Event();
         Integer testPrice = 320;
         testEvent.setGuests("50-100");
         testEvent.setCoupon("coming out");

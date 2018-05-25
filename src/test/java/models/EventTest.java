edcd20f5435104fb72c91b97e27f46099277dacb
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 10:05:38 -0700

    Add passing test for proper Event instantiation

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,7 +1,13 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
-
+    @Test
+    public void newEvent_instantiatesCorrectly_true() {
+        Event testEvent = new Event();
+        assertEquals(true, testEvent instanceof Event);
+    }
 }

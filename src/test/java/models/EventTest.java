75eed5df275ea65ff810907afa83f9aa921a1973
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 11:00:08 -0700

    Add test for instantiating new Event with name passed

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.*;
 
 public class EventTest {
     @Test
-    public void newEvent_instantiatesCorrectly_true() {
-        Event testEvent = new Event();
-        assertEquals(true, testEvent instanceof Event);
+    public void newEvent_instantiatesCorrectly_test() {
+        Event testEvent = new Event("test");
+        assertEquals("test", testEvent.getName());
     }
+
 }

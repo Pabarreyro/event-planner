9fd6809569b7ae2a0beaa70992542f2f23cb70ed
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 11:00:33 -0700

    Add passing code for instantiating Event with name

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -3,9 +3,18 @@ package models;
 import java.util.List;
 
 public class Event {
+    private String name;
     private Integer guests;
     private List<String> food;
     private List<String> drink;
     private List<String> entertainment;
+    private Integer price;
 
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

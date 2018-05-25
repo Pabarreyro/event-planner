d31956df030c75458f481e028aaafe4d3bd97270
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 14:27:28 -0700

    Add greeting and party name prompt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,27 @@
+import models.Event;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Let's plan a party!");
+        boolean quoterRunning = true;
+
+        while (quoterRunning) {
+            try {
+                BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("First things first, let's give your party a name:");
+                String inputName = bufferedReader.readLine();
+
+                Event quotedEvent = new Event(inputName);
+                System.out.println(String.format("How many people are attending %s?", quotedEvent.getName()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }

87f93ca87f3a18d3587cfbc206a97e89abedafdb
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-25 10:03:41 -0700

    Designate mvp class fields

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,11 @@
 package models;
 
+import java.util.List;
+
 public class Event {
+    private Integer guests;
+    private List<String> food;
+    private List<String> drink;
+    private List<String> entertainment;
+
 }
